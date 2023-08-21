9586051900dacdcafb3bad48eb6bee6b5a9ece08
caramelinvestor <nnandibekee@gmail.com>
2023-08-21 15:36:38 +0000

    betty in check

--- def_fprintf.c
+++ def_fprintf.c
@@ -1,7 +1,9 @@
 #include "shell.h"
-
+void def_fprintf(FILE *stream, const char *format, ...);
 /**
- *
+ * def_fprintf - user defined function for fprintf
+ * @stream: pointer to the stream
+ * @format: pointer to formatted string
  */
 
 void def_fprintf(FILE *stream, const char *format, ...)
@@ -11,14 +13,16 @@ void def_fprintf(FILE *stream, const char *format, ...)
 	int result;
 
 	va_list args;
+
 	va_start(args, format);
 
 	/* Allocate memory for the buffer dynamically*/
 	buffer = NULL;
-	buffer_size = def_vsnprintf(NULL, 0, format, args) + 1; /*Get the required size*/ 
+	buffer_size = def_vsnprintf(NULL, 0, format, args) + 1;
 	result = def_vsnprintf(buffer, buffer_size, format, args);
 
-	if (buffer_size <= 0) {
+	if (buffer_size <= 0)
+	{
 		def_fputs("Error: Failed to determine buffer size\n", stream);
 		va_end(args);
 		return;
@@ -26,15 +30,18 @@ void def_fprintf(FILE *stream, const char *format, ...)
 
 	buffer = (char *)malloc(buffer_size);
 
-	if (buffer == NULL) {
+	if (buffer == NULL)
+	{
 		def_fputs("Error: Memory allocation failed\n", stream);
 		va_end(args);
 		return;
 	}
 
-	if (result < 0) {
+	if (result < 0)
+	{
 		def_fputs("Error: Formatted string too long\n", stream);
-	} else {
+	} else
+	{
 		/* Write the formatted string to the stream*/
 		def_fputs(buffer, stream);
 	}
--- main.c
+++ main.c
@@ -30,7 +30,8 @@ int main(void)
 					free(tokens);
 				break;
 			}
-			if (my_strcmp(tokens[0], "pwd") == 0) {
+			if (my_strcmp(tokens[0], "pwd") == 0)
+			{
 				def_current_dir();
 				continue;
 			}
@@ -42,31 +43,39 @@ int main(void)
 				continue;
 			}
 			/* Check for the setenv built-in command */
-			if (my_strcmp(tokens[0], "setenv") == 0) {
-				if (tokens[1] != NULL && tokens[2] != NULL) {
+			if (my_strcmp(tokens[0], "setenv") == 0)
+			{
+				if (tokens[1] != NULL && tokens[2] != NULL)
+				{
 					/* Call def_setenv to set the environment variable */
 					def_setenv(tokens[1], tokens[2]);
-				} else {
+				} else
+				{
 					def_fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
 				}
 				continue;
 			}
 
 			/* Check for the unsetenv built-in command */
-			if (my_strcmp(tokens[0], "unsetenv") == 0) {
-				if (tokens[1] != NULL) {
+			if (my_strcmp(tokens[0], "unsetenv") == 0)
+			{
+				if (tokens[1] != NULL)
+				{
 					/* Call def_unsetenv to unset the environment variable */
 					def_unsetenv(tokens[1]);
-				} else {
+				} else
+				{
 					def_fprintf(stderr, "Usage: unsetenv VARIABLE\n");
 				}
 				continue;
 			}
 			/* Check for other allowed commands */
-			if (def_valid_command(tokens[0])) {
+			if (def_valid_command(tokens[0]))
+			{
 				/* Execute the command */
 				_execme(tokens);
-			} else {
+			} else
+			{
 				/* Display an error message for invalid command */
 				def_fprintf(stderr, "Invalid command: %s\n", tokens[0]);
 			}
